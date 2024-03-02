65b0a44e7c7a8151f35ababceb899aa92a0179b7
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-03-02 12:43:00 -0600

    Card base

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./main.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'very-dark-blue':'hsl(233, 47%, 7%)',
+        'dark-desaturated-blue':'hsl(244, 38%, 16%)',
+        'soft-violet':'hsl(277, 64%, 61%)',
+        't-white':'hsl(0, 0%, 100%)',
+        'p-transparent-white':'hsla(0, 0%, 100%, 0.75)',
+        'h-transparent-white':'hsla(0, 0%, 100%, 0.6)',
+      }
+    },
   },
   plugins: [],
 }
